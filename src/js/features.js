7817be59547ae74c8a6bba358385b1d1800f5a7f
Jeffrey <JeffreyEjeanye@gmail.com>
2022-01-04 21:05:39 +0100

    changed the active-feature line

--- src/js/features.js
+++ src/js/features.js
@@ -1,5 +1,20 @@
 "use strict";
+let active_feature_1 = document.querySelector(".active-feature-container-1");
+let active_feature_2 = document.querySelector(".active-feature-container-2");
+let active_feature_3 = document.querySelector(".active-feature-container-3");
+let check_active_feature = document.querySelector(".active-feature-container-1 > .active-feature");
 
 let features = document.addEventListener("click", e => {
     console.log(e);
-})  
+    if (e.target === active_feature_1) {
+        active_feature_1.insertAdjacentHTML("afterbegin", `<hr class="active-feature_1">`);
+     } else if (e.target === active_feature_2) {
+        active_feature_2.insertAdjacentHTML("afterbegin", `<hr class="active-feature_2">`);
+     } else if (e.target === active_feature_3) {
+        active_feature_3.insertAdjacentHTML("afterbegin", `<hr class="active-feature_3">`);
+     } else {
+         console.log("error");
+     }
+    
+    
+});  
