2b9964f441fd16d1e75db63c9d4ba4e3c77a9195
Jeffrey <JeffreyEjeanye@gmail.com>
2022-01-06 04:07:05 +0100

    tablist fully done

--- src/js/features.js
+++ src/js/features.js
@@ -15,6 +15,70 @@ let active_features_1 = document.querySelector(".active-feature_1");
 let active_features_2 = document.createElement("hr");
 active_features_2.setAttribute("class", "active-feature_2");
 
+// TAB-TEXT
+let text_1 = document.createElement("section");
+text_1.setAttribute("id", "tab-text");
+text_1.innerHTML = `<div id="tab-container">
+  <div class="tab-grid-container">
+    <div class="tab-bg">
+    <div><img src="./src/images/illustration-features-tab-1.svg" alt=""></div>
+  </div>
+  </div>
+</div>
+
+<div class="flex-container tab-text-container">
+  <h1>Bookmark in one click</h1>
+  <p>Organize your bookmarks however you like. Our simple drag-and-drop interface 
+    gives you complete control over how you manage your favourite sites.</p>
+</div>`;
+
+
+let text_2 = document.createElement("section");
+text_2.setAttribute("id", "tab-text");
+text_2.innerHTML = `<section id="tab-text">
+
+<div id="tab-container">
+  <div class="tab-grid-container">
+    <div class="tab-bg">
+    <div><img src="./src/images/illustration-features-tab-2.svg" alt=""></div>
+  </div>
+  </div>
+</div>
+
+<div class="flex-container tab-text-container">
+  <h1>Intelligent search</h1>
+  <p>Our powerful search feature will help you find saved sites in no time at all. 
+    No need to trawl through all of your bookmarks.</p>
+</div>
+
+</section>`;
+
+
+let text_3 = document.createElement("section");
+text_3.setAttribute("id", "tab-text");
+text_3.innerHTML = `<section id="tab-text">
+
+<div id="tab-container">
+  <div class="tab-grid-container">
+    <div class="tab-bg">
+    <div><img src="./src/images/illustration-features-tab-3.svg" alt=""></div>
+  </div>
+  </div>
+</div>
+
+<div class="flex-container tab-text-container">
+  <h1>Share your bookmarks</h1>
+  <p>Easily share your bookmarks and collections with others. Create a shareable 
+    link that you can send at the click of a button.</p>
+</div>
+
+</section>`;
+
+// Inputs the tab 1 on reload
+features.insertAdjacentElement("afterend", text_1);
+
+// TAB-TEXT-REMOVE
+let text_remove_1 = document.querySelector("#tab-text");
 
 let active_features_3 = document.createElement("hr");
 active_features_3.setAttribute("class", "active-feature_3");
@@ -26,17 +90,28 @@ active_features_3.setAttribute("class", "active-feature_3");
         active_features_2.remove();
         active_features_3.remove();
         active_container_1.insertAdjacentElement("afterbegin", active_features_1);
-        
-     }
-
     
-    if (e.target === span_target_3) {
-        active_container_3.insertAdjacentElement("afterbegin", active_features_3);
-        active_features_1.remove();
-        active_features_2.remove();
-        
-     } 
+        text_2.replaceWith(text_1);
+        text_3.replaceWith(text_1);
+    } 
 
+     
+
+
+     if (e.target === span_target_3) {
+        active_container_3.insertAdjacentElement("afterbegin", active_features_3);
+    if (text_1.hasAttribute!== "#tab-text") {
+       text_1.replaceWith(text_3);
+       text_2.replaceWith(text_3);
+    } else {
+        console.log("ups");
+    }
+            
+            active_features_1.remove();
+            active_features_2.remove();
+            
+         }  
+  
 
 });  
 
@@ -49,6 +124,13 @@ features1.addEventListener("click", e => {
            active_container_2.insertAdjacentElement("afterbegin", active_features_2);
        
        }
+
+       if (text_1.hasAttribute!== "#tab-text") {
+       text_1.replaceWith(text_2);
+       text_3.replaceWith(text_2);
+     } else {
+         console.log("ups");
+     }
    };
    create();
    active_features_1.remove();
