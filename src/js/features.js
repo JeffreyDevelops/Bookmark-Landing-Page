2237d4d78ab7e29ca6e609855a7ca0312952ee68
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2022-01-15 22:06:30 +0100

    Working feature area (background changing, size etc)

--- src/js/features.js
+++ src/js/features.js
@@ -1,21 +1,4 @@
 "use strict";
-let features = document.querySelector("#features-area-desktop");
-let features_2 = document.querySelector("#features-area-mobile");
-let span_target_1 = document.querySelector(".header_1")
-let span_target_2 = document.querySelector(".header_2");
-let span_target_3 = document.querySelector(".header_3");
-// ACTIVE CONTAINER 
-let active_container_1 = document.querySelector(".header_1");
-let active_container_2 = document.querySelector(".header_2");
-let active_container_3 = document.querySelector(".header_3");
-// =============================================================================
-let active_features_1 = document.querySelector(".active-feature_1");
-
-let active_features_2 = document.createElement("hr");
-active_features_2.setAttribute("class", "active-feature_2");
-
-let active_features_3 = document.createElement("hr");
-active_features_3.setAttribute("class", "active-feature_3");
 
 // TAB-TEXT
 let text_1 = document.createElement("section");
@@ -76,16 +59,31 @@ text_3.innerHTML = `<section id="tab-text">
 
 </section>`;
 
+
+let features = document.querySelector("#features-area-desktop");
+let features_2 = document.querySelector("#features-area-mobile");
+
+// Mobile
+let header_1 = document.querySelector(".header_1");
+let header_2 = document.querySelector(".header_2");
+let header_3 = document.querySelector(".header_3");
+let active_features_1 = document.querySelector(".active-feature_1");
+let active_features_2 = document.createElement("hr");
+active_features_2.setAttribute("class", "active-feature_2");
+let active_features_3 = document.createElement("hr");
+active_features_3.setAttribute("class", "active-feature_3");
+
+
 // Inputs the tab 1 on reload
 features.insertAdjacentElement("afterend", text_1);
 
 
     features_2.addEventListener("click", e => {
     console.log(e);
-    if (e.target === span_target_1) {
+    if (e.target === header_1) {
         active_features_2.remove();
         active_features_3.remove();
-        active_container_1.insertAdjacentElement("afterend", active_features_1);
+        header_1.insertAdjacentElement("afterend", active_features_1);
     
         text_2.replaceWith(text_1);
         text_3.replaceWith(text_1);
@@ -94,8 +92,8 @@ features.insertAdjacentElement("afterend", text_1);
      
 
 
-     if (e.target === span_target_2) {
-        active_container_2.insertAdjacentElement("afterend", active_features_2);
+     if (e.target === header_2) {
+        header_2.insertAdjacentElement("afterend", active_features_2);
     if (text_1.hasAttribute!== "#tab-text") {
        text_1.replaceWith(text_3);
        text_2.replaceWith(text_3);
@@ -112,12 +110,12 @@ features.insertAdjacentElement("afterend", text_1);
 });  
 
 
-span_target_3.addEventListener("click", e => {
+header_3.addEventListener("click", e => {
     let create = function () {
-       if (active_container_3.contains === ".active-feature_3") {
+       if (header_3.contains === ".active-feature_3") {
            console.log("yes i have it");
-       } else if (active_container_3.hasAttribute !== ".active-feature_3") {
-           active_container_3.insertAdjacentElement("afterend", active_features_3);
+       } else if (header_3.hasAttribute !== ".active-feature_3") {
+           header_3.insertAdjacentElement("afterend", active_features_3);
        
        }
 
@@ -135,15 +133,23 @@ span_target_3.addEventListener("click", e => {
 
 
 
-// Desktop 
+// Desktop
+let header_1_desktop = document.querySelector(".header_1_desktop");
+let header_2_desktop = document.querySelector(".header_2_desktop");
+let header_3_desktop = document.querySelector(".header_3_desktop");
+let active_features_1_desktop = document.querySelector(".active-feature_1_desktop");
+let active_features_2_desktop = document.createElement("hr");
+active_features_2_desktop.setAttribute("class", "active-feature_2_desktop");
+let active_features_3_desktop = document.createElement("hr");
+active_features_3_desktop.setAttribute("class", "active-feature_3_desktop");
 
 
 features.addEventListener("click", e => {
   console.log(e);
-  if (e.target === span_target_1) {
-      active_features_2.remove();
-      active_features_3.remove();
-      active_container_1.insertAdjacentElement("afterend", active_features_1);
+  if (e.target === header_1_desktop) {
+      active_features_2_desktop.remove();
+      active_features_3_desktop.remove();
+      header_3_desktop.insertAdjacentElement("afterend", active_features_1_desktop);
   
       text_2.replaceWith(text_1);
       text_3.replaceWith(text_1);
@@ -152,8 +158,8 @@ features.addEventListener("click", e => {
    
 
 
-   if (e.target === span_target_2) {
-      active_container_2.insertAdjacentElement("afterend", active_features_2);
+   if (e.target === header_2_desktop) {
+      header_3_desktop.insertAdjacentElement("afterend", active_features_2_desktop);
   if (text_1.hasAttribute!== "#tab-text") {
      text_1.replaceWith(text_3);
      text_2.replaceWith(text_3);
@@ -161,8 +167,8 @@ features.addEventListener("click", e => {
       console.log("ups");
   }
           
-          active_features_1.remove();
-          active_features_3.remove();
+          active_features_1_desktop.remove();
+          active_features_3_desktop.remove();
           
        }  
 
@@ -170,12 +176,12 @@ features.addEventListener("click", e => {
 });  
 
 
-span_target_3.addEventListener("click", e => {
+header_3_desktop.addEventListener("click", e => {
   let create = function () {
-     if (active_container_3.contains === ".active-feature_3") {
+     if (header_3_desktop.contains === ".active-feature_3_desktop") {
          console.log("yes i have it");
-     } else if (active_container_3.hasAttribute !== ".active-feature_3") {
-         active_container_3.insertAdjacentElement("afterend", active_features_3);
+     } else if (header_3_desktop.hasAttribute !== ".active-feature_3_desktop") {
+         header_3_desktop.insertAdjacentElement("afterend", active_features_3_desktop);
      
      }
 
@@ -187,8 +193,8 @@ span_target_3.addEventListener("click", e => {
    }
  };
  create();
- active_features_1.remove();
- active_features_2.remove();
+ active_features_1_desktop.remove();
+ active_features_2_desktop.remove();
 }); 
 
 
